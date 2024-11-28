4be038f6f3d9b7986a8c4cb0771514b5287a42f8
UlyaBurgasova <ulya.burgasova2511@yandex.ru>
2024-11-28 20:40:05 +0300

    Add files via upload

---
+++ burger/php/main.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/main/main.css">
+    <link rel="shortcut icon" href="/img/BurgerIcon.ico"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Primary list</title>
+</head>
+<body>
+    <?php include "..\blocks\header.php"; ?>
+    <header>
+        <img src="/img//img/main_img.jpeg" alt="" width="100%">
+    </header>
+
+    <div class="spine">
+        <div class="container1">
+            <div class="box women"><img src="/img//main/women1.jpg" alt="" width="100%" height="100%"></div>
+            <div class="box info">
+                <h2>А ВОТ ЭТО УЖЕ ИНТЕРЕСНО</h2>
+                <p>В нашем ресторане специальное недельное меню, которое не даст вам наскучиться одним и тем же вкусом бургеров.</p>
+                <div class="butn"><p>Узнать подробнее</p></div>
+            </div>
+        </div>
+        <h3>МЕНЮ</h3>
+        <div class="container2">
+            <div class="position">
+                <div class="photo1"> <img src="/img/Style_Double.png" width="100%"></div>
+                <div class="description"><p>Бургеры</p></div>
+            </div>
+            <div class="position">
+                <div class="photo2"><img src="/img/poteto_freeeee.png" width="100%"></div>
+                <div class="description"><p>Гарниры</p></div>
+            </div>
+            <div class="position">
+                <div class="photo3"><img src="/img/cookies.png" width="100%"></div>
+                <div class="description"><p>Десерты</p></div>
+            </div>
+            <div class="position">
+                <div class="photo4"><img src="/img/sodawater.png" width="100%"></div>
+                <div class="description"><p>Напитки</p></div>
+            </div>
+        </div>
+        <h3>ЗАКАЗЫ</h3>
+        <div class="container3">
+            <div class="phase1">
+                <div class="order">
+                    <h2>НЕТ ВРЕМЕНИ НА ЗАКАЗ?</h2>
+                    <p>Оформи доставку в любую точку города*</p>
+                    <p class="star">*в зависимости от расположения время заказа может измениться</p>
+                    <div class="butn"><p>Узнать подробнее</p>
+                </div>
+                </div>
+                <div class="burger"><img src="/img//main/burger_on_hand.jpg" alt="" width="100%" style="overflow: hidden;height:100%;"></div>
+            </div>
+            <div style="position:relative;overflow:hidden;"><a href="https://yandex.ru/maps/org/kazanskaya_naberezhnaya/206821868201/?utm_medium=mapframe&utm_source=maps" style="color:#eee;font-size:12px;position:absolute;top:0px;"></a><a href="https://yandex.ru/maps/15/tula/category/landmark_attraction/89683368508/?utm_medium=mapframe&utm_source=maps" style="color:#eee;font-size:12px;position:absolute;top:14px;">Достопримечательность в Туле</a><iframe src="https://yandex.ru/map-widget/v1/?ll=37.620508%2C54.195835&mode=poi&poi%5Bpoint%5D=37.621148%2C54.196220&poi%5Buri%5D=ymapsbm1%3A%2F%2Forg%3Foid%3D206821868201&z=18.63" width="560" height="400" frameborder="1" allowfullscreen="true" style="position:relative;border-radius:0.75rem;width:99.9%;height:99.9%;border:none;"></iframe></div>
+        </div>
+    </div>
+    <div class="last_pre">
+            <div class="left"><img src="/img//about_us/potato7.jpg" alt="potatoBlue" width="100%" height="100%"></div>
+            <div class="right"><img src="/img//about_us/burgerParts.jpg" alt="burgerPink" width="100%" height="100%"></div>
+    </div>
+
+
+
+    
+    <?php include '..\blocks\footer.php'; ?>
+
+</body>
+</html>
